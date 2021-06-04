a74bde48e1a385cc978fbf9eac23aeab511c7a56
Mustafa Birhat Gemsiz <birhatgemsiz@hotmail.com>
2021-06-04 12:41:45 +0300

    finish

---
+++ SmtpMail/FrmMail.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace SmtpMail
+{
+    partial class FrmGmail
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGmail));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtKime = new System.Windows.Forms.TextBox();
+            this.TxtKonu = new System.Windows.Forms.TextBox();
+            this.RchIcerik = new System.Windows.Forms.RichTextBox();
+            this.BtnGonder = new System.Windows.Forms.Button();
+            this.BtnTemizle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Thistle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-3, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1082, 142);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(358, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtKime
+            // 
+            this.TxtKime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtKime.Location = new System.Drawing.Point(318, 256);
+            this.TxtKime.Name = "TxtKime";
+            this.TxtKime.Size = new System.Drawing.Size(498, 27);
+            this.TxtKime.TabIndex = 3;
+            // 
+            // TxtKonu
+            // 
+            this.TxtKonu.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtKonu.Location = new System.Drawing.Point(318, 296);
+            this.TxtKonu.Name = "TxtKonu";
+            this.TxtKonu.Size = new System.Drawing.Size(498, 27);
+            this.TxtKonu.TabIndex = 4;
+            // 
+            // RchIcerik
+            // 
+            this.RchIcerik.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RchIcerik.Location = new System.Drawing.Point(318, 336);
+            this.RchIcerik.Name = "RchIcerik";
+            this.RchIcerik.Size = new System.Drawing.Size(498, 207);
+            this.RchIcerik.TabIndex = 5;
+            this.RchIcerik.Text = "";
+            // 
+            // BtnGonder
+            // 
+            this.BtnGonder.BackColor = System.Drawing.Color.PaleGreen;
+            this.BtnGonder.FlatAppearance.BorderSize = 0;
+            this.BtnGonder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGonder.Location = new System.Drawing.Point(603, 565);
+            this.BtnGonder.Name = "BtnGonder";
+            this.BtnGonder.Size = new System.Drawing.Size(100, 46);
+            this.BtnGonder.TabIndex = 9;
+            this.BtnGonder.Text = "Gönder";
+            this.BtnGonder.UseVisualStyleBackColor = false;
+            this.BtnGonder.Click += new System.EventHandler(this.BtnGonder_Click);
+            // 
+            // BtnTemizle
+            // 
+            this.BtnTemizle.BackColor = System.Drawing.Color.Gold;
+            this.BtnTemizle.FlatAppearance.BorderSize = 0;
+            this.BtnTemizle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnTemizle.Location = new System.Drawing.Point(716, 565);
+            this.BtnTemizle.Name = "BtnTemizle";
+            this.BtnTemizle.Size = new System.Drawing.Size(100, 46);
+            this.BtnTemizle.TabIndex = 10;
+            this.BtnTemizle.Text = "Temizle";
+            this.BtnTemizle.UseVisualStyleBackColor = false;
+            this.BtnTemizle.Click += new System.EventHandler(this.BtnTemizle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(256, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 28);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Kime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(256, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 28);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Konu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(256, 336);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 28);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "İçerik";
+            // 
+            // FrmGmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(1074, 718);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTemizle);
+            this.Controls.Add(this.BtnGonder);
+            this.Controls.Add(this.RchIcerik);
+            this.Controls.Add(this.TxtKonu);
+            this.Controls.Add(this.TxtKime);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmGmail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gmail Uygulaması";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TxtKime;
+        private System.Windows.Forms.TextBox TxtKonu;
+        private System.Windows.Forms.RichTextBox RchIcerik;
+        private System.Windows.Forms.Button BtnGonder;
+        private System.Windows.Forms.Button BtnTemizle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SmtpMail/FrmMail.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmtpMail
+{
+    public partial class FrmGmail : Form
+    {
+        void epostaGonder(String kime, String konu, String icerik)
+        {
+            String kimden = "ornek@gmail.com";
+            MailMessage message = new MailMessage(kimden, kime);
+            message.Subject = konu;
+            message.Body = icerik;
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.Port = 587;
+            NetworkCredential epostabilgileri = new NetworkCredential("ornek@gmail.com", "hesap-parolası");
+            client.Credentials = epostabilgileri;
+            client.EnableSsl = true;
+            
+            try
+            {
+                client.SendAsync(message,null);
+                MessageBox.Show("Başarılı şekilde göderildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        public FrmGmail()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGonder_Click(object sender, EventArgs e)
+        {
+            epostaGonder(TxtKime.Text, TxtKonu.Text, RchIcerik.Text);
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            TxtKime.ResetText();
+            TxtKonu.ResetText();
+            RchIcerik.Clear();
+        }
+    }
+}
